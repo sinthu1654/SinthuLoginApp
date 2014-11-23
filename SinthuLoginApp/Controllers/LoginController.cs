509e1b144d2e50aa516e1e47191c345340c7eee1
Sinthu <sinthu1654@gmail.com>
2014-11-23 22:21:15 +0530

    Commit solutions

---
+++ SinthuLoginApp/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SinthuLoginApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                //if (model.Username == "Sinthu" && model.Password == "1234")
+                if (DataAccess.DAL.IsValidUser(model.Username, model.Password ))
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false); 
+                    //change to true, so browser remembers the user login session
+                    return RedirectToAction("index", "home");
+                }
+                {
+                    ModelState.AddModelError("", "Invalid Username / Password");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ SinthuLoginApp/DataAccess/DAL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace SinthuLoginApp.DataAccess
+{
+    public class DAL
+    {
+        static SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDbConnection"].ToString());
+
+        public static bool IsValidUser (string username, string password)
+        {
+            bool authenthicated = false;
+
+            string query = string.Format("SELECT * FROM [User] WHERE Username = '{0}' AND Password = '{1}'", username, password);
+
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            authenthicated = sdr.HasRows;
+            conn.Close();
+            return (authenthicated);
+        }
+    }
+}
